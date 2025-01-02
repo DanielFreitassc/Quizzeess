0177d72170925f8a3ce683108b0e884f7138cb62
DanielFreitassc <danielfreitassc@gmail.com>
2025-01-02 15:17:13 -0300

    Fix: Cors

--- backend/src/main/java/com/danielfreitassc/backend/configurations/CorsConfig.java
+++ backend/src/main/java/com/danielfreitassc/backend/configurations/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET","POST","PUT","DELETE","PATCH");
+        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET","POST","PUT","DELETE","PATCH");
     }
     
 }
